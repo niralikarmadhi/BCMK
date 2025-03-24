e0a69aefc70ee381f6788b0e82f9a2f433b03706
varsha <varsha@karmadhi.com>
2025-03-24 12:46:07 +0530

    home page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20450,7 +20450,32 @@ var Parts = /*#__PURE__*/function () {
   }
   parts_createClass(Parts, [{
     key: "init",
-    value: function init() {}
+    value: function init() {
+      this.LeftRightImg();
+    }
+  }, {
+    key: "LeftRightImg",
+    value: function LeftRightImg() {
+      $(document).ready(function () {
+        // Get the first option's image and set it as the default
+        var defaultImg = $(".what-we-offer-option").first().data("img");
+        $("#offer-img").attr("src", defaultImg);
+
+        // On hover, change image
+        $(".what-we-offer-option").hover(function () {
+          var newImg = $(this).data("img");
+          $("#offer-img").attr("src", newImg);
+        }, function () {
+          // Reset to the first option's image when mouse leaves
+          $("#offer-img").attr("src", defaultImg);
+        });
+
+        // Optional: Reset if mouse leaves the entire group
+        $(".what-we-offer-group").mouseleave(function () {
+          $("#offer-img").attr("src", defaultImg);
+        });
+      });
+    }
   }]);
   return Parts;
 }();
